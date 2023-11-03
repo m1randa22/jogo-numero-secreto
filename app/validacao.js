ab2dbdba455ba12622734d91866d65b8d06e8214
Ester Miranda <118558373+m1randa22@users.noreply.github.com>
2023-11-03 10:23:05 -0300

    Add files via upload

---
+++ app/validacao.js
@@ -0,0 +1,36 @@
+function verificaSeOChutePossuiUmValorValido(chute){
+    const numero = +chute//converte o número para um número inteiro
+
+    if(Number.isNaN(numero)){
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+    }
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
+        elementoChute.innerHTML += `<div>Valor inválido: Diga um número entre ${menorValor} e ${maiorValor}</div>`
+    }
+    if(numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
